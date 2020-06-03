c08ff4558d2b372c1794c0a7ef384af2b10b1893
Stephen Kostiw <steve.kostiw@gmail.com>
2020-06-03 17:43:34 -0400

    Added base for generic AI component

---
+++ Assets/Scripts/AI/GenericAIComponent.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericAIComponent : MonoBehaviour
+{
+    protected ActionData.ACTION_LIST_ID m_CurrentSelectedActionID = ActionData.ACTION_LIST_ID.NONE;
+    protected ActionData.ACTION_LIST_ID m_PreviousSelectedActionID = ActionData.ACTION_LIST_ID.NONE;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public PerformActionDataModel CreatePerformActionDataModel(ActionData.ACTION_LIST_ID aActionID)
+    {
+        GenericActionModel actionModel = ActionData.ABILITY_DICTIONARY[aActionID];
+        PerformActionDataModel actionDataModel = null;
+
+        switch (actionModel.GetActionTargetAmount())
+        {
+            case GenericActionModel.ACTION_TARGET_AMOUNT.ALL_TARGETS:
+                actionDataModel = new PerformActionDataModel(aActionID, GenericActionModel.ACTION_TARGET_AMOUNT.ALL_TARGETS, GetComponentInParent<GenericCharacterController>().GetCharacterStats());
+                break;
+
+            case GenericActionModel.ACTION_TARGET_AMOUNT.MULTI_TARGET_DEFENSIVE:
+                actionDataModel = new PerformActionDataModel(aActionID, GenericActionModel.ACTION_TARGET_AMOUNT.MULTI_TARGET_DEFENSIVE, GetComponentInParent<GenericCharacterController>().GetCharacterStats());
+                break;
+
+            case GenericActionModel.ACTION_TARGET_AMOUNT.MULTI_TARGET_OFFENSIVE:
+                actionDataModel = new PerformActionDataModel(aActionID, GenericActionModel.ACTION_TARGET_AMOUNT.MULTI_TARGET_OFFENSIVE, GetComponentInParent<GenericCharacterController>().GetCharacterStats());
+                break;
+
+            case GenericActionModel.ACTION_TARGET_AMOUNT.SINGLE_TARGET:
+                GenericCharacter defender;
+
+                if (actionModel.GetActionOffOrDef() == GenericActionModel.ACTION_OFF_OR_DEF.DEFENSIVE)
+                {
+                    defender = GetRandomNonPlayerCharacter();
+                }
+                else
+                {
+                    defender = GetRandomPlayerCharacter();
+                }
+
+                actionDataModel = new PerformActionDataModel(aActionID, GenericActionModel.ACTION_TARGET_AMOUNT.SINGLE_TARGET, GetComponentInParent<GenericCharacterController>().GetCharacterStats(), defender);
+                break;
+
+            case GenericActionModel.ACTION_TARGET_AMOUNT.NO_TARGETS:
+            default:
+                Debug.Log("ERROR: Tried to create an action model through the AI component but the target type is none or null.");
+                break;
+            
+        }
+
+        return actionDataModel;
+    }
+
+    public void DecideActionToUseAlgorithm()
+    {
+        m_CurrentSelectedActionID = ActionData.ACTION_LIST_ID.NONE;
+    }
+
+    public GenericCharacter GetRandomNonPlayerCharacter()
+    {
+        GenericCharacter character = null;
+        bool bCharFound = false;
+
+        while (bCharFound == false)
+        {
+            int randIndex = Random.Range(0, GameManager.GetCombatManager.m_AllActionUsers.Count);
+
+            if (!GameManager.GetCombatManager.m_AllActionUsers[randIndex].IsPlayerControlled() &&
+                GetComponentInParent<GenericCharacterController>().GetCharacterStats() != GameManager.GetCombatManager.m_AllActionUsers[randIndex])
+            {
+                bCharFound = true;
+
+                character = GameManager.GetCombatManager.m_AllActionUsers[randIndex];
+            }
+        }
+
+        return character;
+    }
+
+    public GenericCharacter GetRandomPlayerCharacter()
+    {
+        GenericCharacter character = null;
+        bool bCharFound = false;
+
+        while (bCharFound == false)
+        {
+            int randIndex = Random.Range(0, GameManager.GetCombatManager.m_AllActionUsers.Count);
+
+            if (GameManager.GetCombatManager.m_AllActionUsers[randIndex].IsPlayerControlled() &&
+                GetComponentInParent<GenericCharacterController>().GetCharacterStats() != GameManager.GetCombatManager.m_AllActionUsers[randIndex])
+            {
+                bCharFound = true;
+
+                character = GameManager.GetCombatManager.m_AllActionUsers[randIndex];
+            }
+        }
+
+        return character;
+    }
+
+}
---
+++ Assets/Scripts/TestScripts/Test_Enemy_1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Enemy_1 : GenericCharacterController
+{
+    public void Init()
+    {
+        GenericCharacter genericCharacter = new GenericCharacter();
+        genericCharacter.SetIsPlayerControlled(false);
+        genericCharacter.SetCharacterName("Enemy");
+        genericCharacter.SetCharacterMana(50);
+        genericCharacter.SetSpriteFileName("elf_male.png");
+        genericCharacter.SetSpriteFilePath("Test_Assets");
+        genericCharacter.AddActionIDToUsableActionList(ActionData.ACTION_LIST_ID.HEAL_TARGET);
+        genericCharacter.AddActionIDToUsableActionList(ActionData.ACTION_LIST_ID.MULTI_STRIKE);
+        genericCharacter.AddActionIDToUsableActionList(ActionData.ACTION_LIST_ID.STRIKE);
+
+        SetCharacterStats(genericCharacter);
+
+        GameManager.GetPlayerManager.AddCharacterToList(genericCharacter);
+    }
+
+    //List of action order to use
+    //tracker for last action used
+    //reset back to beginning if at the end
+    //Simplest AI in that regards
+}
