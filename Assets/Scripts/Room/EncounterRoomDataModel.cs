cb3dc68c0337e4c093e9feba8f06758765273edc
Stephen Kostiw <steve.kostiw@gmail.com>
2020-06-10 17:18:37 -0400

    Added base generic for room data model and encounter room datamodel

---
+++ Assets/Scripts/Room/EncounterRoomDataModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncounterRoomDataModel : GenericRoomDataModel
+{
+    //TODO:
+    //Refactor this to use ID's of enemies in order to save memory.
+    private List<GameObject> m_ListOfEnemiesToSpawn = new List<GameObject>();
+
+    private List<ItemData.ITEM_ID> m_ListOfItemDrops = new List<ItemData.ITEM_ID>();
+
+    private int m_CurrencyDropAmount = 0;
+
+    //TODO: Refactor when ID's for enemies are added
+    public void AddEnemyToSpawnList(GameObject aEnemyObject)
+    {
+        m_ListOfEnemiesToSpawn.Add(aEnemyObject);
+    }
+
+    //Use json to create/populate later at some point
+    public void JSON_CreateEncounterRoomDataModel()
+    {
+        //Not in use right now cause json not implemented
+    }
+
+    public void AddItemToDropList(ItemData.ITEM_ID aItemID)
+    {
+        m_ListOfItemDrops.Add(aItemID);
+    }
+
+    public void SetCurrencyDropAmount(int aAmount)
+    {
+        m_CurrencyDropAmount = aAmount;
+    }
+
+    public int GetCurrencyDropAmount()
+    {
+        return m_CurrencyDropAmount;
+    }
+
+    public void SetCurrencyDropRandomAmountBetweenValues(int aMin, int aMax)
+    {
+        m_CurrencyDropAmount = Random.Range(aMin, aMax);
+    }
+}
---
+++ Assets/Scripts/Room/GenericRoomDataModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class GenericRoomDataModel
+{
+    public enum ROOM_TYPE
+    {
+        ENCOUNTER,
+        SHOP,
+        REST,
+
+
+        NONE
+    }
+
+    protected ROOM_TYPE m_RoomType = ROOM_TYPE.NONE;
+
+    protected string m_BackgroundFilePath = "";
+    protected string m_BackgroundFileName = "";
+
+    public ROOM_TYPE GetRoomType()
+    {
+        return m_RoomType;
+    }
+
+    public void SetRoomType(ROOM_TYPE aRoomType)
+    {
+        m_RoomType = aRoomType;
+    }
+
+    public string GetFilePath()
+    {
+        return m_BackgroundFilePath;
+    }
+
+    public void SetFilePath(string aFilePath)
+    {
+        m_BackgroundFilePath = aFilePath;
+    }
+
+    public string GetFileName()
+    {
+        return m_BackgroundFileName;
+    }
+
+    public void SetFileName(string aFileName)
+    {
+        m_BackgroundFileName = aFileName;
+    }
+}
